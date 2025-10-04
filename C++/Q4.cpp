ae9474508dbe3d5a298c9d56817cbe81196cf39b
Manav Gosain <k.gosain7140@gmail.com>
2025-10-04 13:46:30 +0530

    added Q3, Q4 and Q5 in Java, C++ and Pythoin respectively

---
+++ C++/Q4.cpp
@@ -0,0 +1,10 @@
+// Program to Multiply two Floating Point Numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x = 23.5;
+    float y = 34.6;
+    cout << x * y;
+    return 0;
+}
