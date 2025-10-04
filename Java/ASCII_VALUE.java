526688dff09bf0fdabefb646c280e6f36781b588
sahil07-git <sahilbhatkande231@gmail.com>
2025-10-04 17:22:04 +0530

    Add Simple-DSA code including ASCII_VALUE.java

---
+++ Java/ASCII_VALUE.java
@@ -0,0 +1,10 @@
+// package Simple-DSA.Java;
+
+public class ASCII_VALUE {
+    public static void main(String[] args) {
+        char character = 'A'; // You can change this to any character
+        int asciiValue = (int) character;
+        System.out.println("The ASCII value of '" + character + "' is: " + asciiValue);
+    }
+    
+}
