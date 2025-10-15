b63d55401da93b298ebd01e8bb5193916eb56749
Mokshii46 <mokshishah0501@gmail.com>
2025-10-16 00:29:20 +0530

    Added Q27,28 of java

---
+++ Java/Q28.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false; 
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a Prime number.");
+        else
+            System.out.println(num + " is NOT a Prime number.");
+
+        sc.close();
+    }
+}
