fda5a704c6c8fc52638fc69e09d92eedfcab301e
mehaksharma25 <sharmamehak2503@gmail.com>
2025-10-23 16:59:39 +0530

    Create Calculate_Power.js

---
+++ javascript/Calculate_Power.js
@@ -0,0 +1,9 @@
+/ Program to calculate power using ** operator
+
+function calculatePower(base, exponent) {
+  const result = base ** exponent;
+${base} ^ ${exonent} = ${result}`);
+}
+
+// Exampe usage:
+calculatePower(3, 2);  // Output: 3 ^ 2 = 9
